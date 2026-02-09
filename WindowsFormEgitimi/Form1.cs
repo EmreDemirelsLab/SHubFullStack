a53bd3a5e859057940fbc398659dce1266266cc4
Emre Demirel <h.emredemirel@gmail.com>
2026-02-09 23:09:51 +0300

    09-02-2026

---
+++ WindowsFormEgitimi/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormEgitimi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // bu metod form açılırken çalışır.
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "Butona Tıklandı!";
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                button1.Enabled = true;
+            }
+            else
+            {
+                button1.Enabled = false;
+            }
+        }
+    }
+}
+
